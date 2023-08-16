e9dcb90441ba823032413d7b32beea2ac6d1008a
Ananasovna <a.n.ushkova@yandex.ru>
2023-08-17 00:16:29 +0200

    fix: fix mistake in reducer logic

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: any): any => {
     switch (action.type) {
         case 'sort': {
-            return [...state.sort((a, b) => {
+            return [...state].sort((a, b) => {
                 if (a.name > b.name) return (action.payload === 'up') ? 1 : -1;
                 if (a.name < b.name) return (action.payload === 'up') ? -1 : 1;
                 return 0;
-            })]
+            })
         }
         case 'check': {
-            return state.filter(el => el.age >= 18);
+            return state.filter(el => el.age >= action.payload);
         }
         default:
             return state
